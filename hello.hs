edb838ca5f9e1e57434d87c358c9c68045853a4c
can chen <cancorreo1001@gmail.com>
2024-04-03 21:29:59 +0200

    HTML printer library:: Module Html :: CREATE

--- hello.hs
+++ hello.hs
@@ -1,24 +1,18 @@
-main = putStrLn myhtml2
+import Prelude ( IO, putStrLn )
+import Html
 
--- wrapHtml content = "<html><body>" <> content <> "</body></html>"
--- myhtml = wrapHtml "Hello, world!"
+main :: IO ()
+main = putStrLn (render myHtml)
 
-
--- ejer 1 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
-
--- ejer 2
--- myhtml = html_ ( body_ "My page title" )
-myhtml = html_ ( body_ "My page title" )
-
-
--- ejer 3
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>"  <> content <> "</title>"
-
--- ejer 4 
-makeHtml title content = html_ (head_ (title_ title) <> body_ content) 
- 
-myhtml2 = makeHtml "Hello title" "Hello, world!"
-
+-- myHtml
+myHtml :: Html
+myHtml = 
+    html_
+        "Title"
+        ( append_
+                (h1_ "h1")
+                (append_
+                    (p_ "Paragraph 1")
+                    (p_ "Paragraph 2")
+                )
+        )
