685eefcdd1e800972bb7c05e239a2c09f0a89ebe
Can Chen <cancorreo1001@gmail.com>
2024-04-18 18:07:34 +0200

    Markup.hs clean

--- Markup.hs
+++ Markup.hs
@@ -1,14 +1,13 @@
-
 module Markup
-(
-  Document
+  ( Document
   , Structure(..)
   , parse
-)
+  )
 where
 
 import Numeric.Natural
-import Data.Maybe (maybeToList) 
+import Data.Maybe (maybeToList)
+
 type Document
   = [Structure]
 
@@ -18,87 +17,64 @@ data Structure
   | UnorderedList [String]
   | OrderedList [String]
   | CodeBlock [String]
-  deriving(Eq, Show)
+  deriving (Eq, Show)    -- (1)
 
--- -- Hello, world!
--- ex1 :: Document
--- ex1 = 
---     [ Paragraph "Hello, world!"
---     ]
-
--- ex2 :: Document
--- ex2 = 
---     [ Heading 1 "Welcome"
---     , Paragraph "To this tutorial about Haskell."
---     ]
-
--- ex3 :: Document
--- ex3 = 
---     [ Paragraph "Remember that multiple lines with no separation are grouped together into a single paragraph but list items remain separate."
---     , UnorderedList
---         [ "Item 1 of a list"
---         , " Item 2 of the same list"
---         ]
---     ]
-
--- ex4 = 
---     [ Heading 1 "Compiling programs with ghc"
---     , Paragraph "Running ghc invokes the Glasgow Haskell Compiler (GHC), and can be used to compile Haskell modules and programs into native executables and libraries."
---     , Paragraph "Create a new Haskell source file named hello.hs, and write the following code in it:"
---     , CodeBlock ["main = putStrLn \"Hello, Haskell!\""]
---     , Paragraph "Now, we can compile the program by invoking ghc with the file name:"
---     , CodeBlock 
---         [ "➜ ghc hello.hs"
---         , "[1 of 1] Compiling Main             ( hello.hs, hello.o )"
---         , "Linking hello ..."
---         ]
---     , Paragraph "GHC created the following files:"
---     , UnorderedList 
---         [ "hello.hi - Haskell interface file"
---         , "hello.o - Object file, the output of the compiler before linking"
---         , "hello (or hello.exe on Microsoft Windows) - A native runnable executable."
---         ]
---     , Paragraph "GHC will produce an executable when the source file satisfies both conditions:"
---     , OrderedList
---         [ "Defines the main function in the source file"
---         , "Defines the module name to be Main or does not have a module declaration"
---         ]
---     , Paragraph "Otherwise, it will only produce the .o and .hi files."
---     ]
 
 parse :: String -> Document
 parse = parseLines Nothing . lines
 
-trim = unwords . words 
-
 parseLines :: Maybe Structure -> [String] -> Document
-parseLines context texts = 
-  case texts of
+parseLines context txts =
+  case txts of
+    -- done case
     [] -> maybeToList context
 
-    ('*': ' ' : line) : rest ->
-      maybe id (:) context (Heading 1 line : parseLines Nothing rest)
+    -- Heading 1 case
+    ('*' : ' ' : line) : rest ->
+      maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+
+    -- Unordered list case
     ('-' : ' ' : line) : rest ->
       case context of
-        Just(UnorderedList uls) -> 
-          parseLines (Just (UnorderedList (line : uls))) rest
+        Just (UnorderedList list) ->
+          parseLines (Just (UnorderedList (list <> [trim line]))) rest
+
         _ ->
           maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
-    currentLine : rest -> 
-      if trim currentLine == ""
-        then maybe id (:) context (parseLines Nothing rest)
-        else
-          case context of
-            Just ( Paragraph t) -> 
-              parseLines (Just(Paragraph (unwords [t, currentLine]))) rest
-            _ -> 
-              maybe id (:) context (parseLines (Just (Paragraph currentLine)) rest)
 
+    -- Ordered list case
+    ('#' : ' ' : line) : rest ->
+      case context of
+        Just (OrderedList list) ->
+          parseLines (Just (OrderedList (list <> [trim line]))) rest
 
+        _ ->
+          maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
 
+    -- Code block case
+    ('>' : ' ' : line) : rest ->
+      case context of
+        Just (CodeBlock code) ->
+          parseLines (Just (CodeBlock (code <> [line]))) rest
 
+        _ ->
+          maybe id (:) context (parseLines (Just (CodeBlock [line])) rest)
 
+    -- Paragraph case
+    currentLine : rest ->
+      let
+        line = trim currentLine
+      in
+        if line == ""
+          then
+            maybe id (:) context (parseLines Nothing rest)
+          else
+            case context of
+              Just (Paragraph paragraph) ->
+                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
+              _ ->
+                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
 
-
-
+trim :: String -> String
+trim = unwords . words
 
